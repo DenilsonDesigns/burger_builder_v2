a5e721952a3340253906741acadd87b013341f86
DenilsonDesigns <daniellanguilller@gmail.com>
2018-11-06 11:58:18 +1100

    added navbar

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "./Logo.module.css";
+import BurgerLogo from "../../assets/images/burger-logo.png";
+
+const Logo = () => {
+  return (
+    <div className={classes.Logo}>
+      <img src={BurgerLogo} alt="MyBurger Logo" />
+    </div>
+  );
+};
+
+export default Logo;
