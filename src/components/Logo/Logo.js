e5f7ea64f856d9c466dafe297ab18c79be46aacf
DenilsonDesigns <daniellanguilller@gmail.com>
2018-11-06 20:31:12 +1100

    added sidedrawer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Logo.module.css";
 import BurgerLogo from "../../assets/images/burger-logo.png";
 
-const Logo = () => {
+const Logo = props => {
   return (
     <div className={classes.Logo}>
       <img src={BurgerLogo} alt="MyBurger Logo" />
