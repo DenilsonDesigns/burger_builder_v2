9ac40efd6a2ee589ba3ea9e69ae527c14d2bf135
DenilsonDesigns <daniellanguilller@gmail.com>
2018-11-09 00:03:20 +1100

    added contact form

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -101,32 +101,21 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({
-      loading: true
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          "=" +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+    queryParams.push("price=" + this.state.totalPrice);
+
+    const queryString = queryParams.join("&");
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString
     });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Daniel Schwarzmuller",
-        address: {
-          street: "BallsVille St",
-          zipCode: "90210",
-          country: "Germania"
-        },
-        email: "balls@balls.com"
-      },
-      deliveryMethod: "fastest"
-    };
-    //send to backend
-    axios
-      .post("/orders.json", order)
-      .then(res => {
-        this.setState({ loading: false, purchasing: false });
-      })
-      .catch(err => {
-        this.setState({ loading: false, purchasing: false });
-      });
   };
 
   render() {
